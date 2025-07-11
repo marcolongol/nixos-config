0122cd719c4d96be33c0a0170f60a755f5c792fd
Lucas Marcolongo <lucas_marco@live.com>
2025-07-10 18:31:33 -0700

    chore: add git signingKey

--- users/lucas/default.nix
+++ users/lucas/default.nix
@@ -44,13 +44,14 @@
     userName = "Lucas Marcolongo";
     userEmail = "lucas_marco@live.com";
     extraConfig = {
+      user.signingKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDNFeQPhPMI2W5Ot5B+Cs7XK5F2TmvjsAiLVD0Ix+tM8AUKjmjLc/Fvt3UvPqY+U09yfu1S6QTL41Q0xQln8MUU+r5DLWh01UiA4obC/m76MGP1Vv3tGubP6+18bJXxNXB7vhPsy5ruTjgAsbXep3PVKjjqZfdrKAiLBF0Zj380KOapZVTDf51vS5ClEFSTX6xgX6VB3rtjYs8aB2GxpAIniLTOmfhkednYhs3bdNRv/MLO0J6SL4+Z//gEg4bq9VsYZviOulBLt2176kk4fyPc2azdIII2Apk2Ulqr9RGvIJ5LVbNpsKDoLzEYPFW/sJ82z4o9uSYXMruKSQWrqkCo5GxsGAi7xTGcVFO0wKPxr3wWZW+K35V7jyWAJFamsnXQUoeXlfIu/DzUfgJ+TSILWWr6ZrV/y0ohkSp9hs1ByDsjmmLEI5SZDjDDs07zaEa6VO9eDuh3xsqB8s2vsaJ+fegDKxm08//rltAXhSx45EADCOzsCYNXteP8eexuqgBQAL11OSCKp6PX4SGDGnu9dip0jpDzZqVpY/AcIxFJsY7niKw+FIsT8lM+4Gxm4w3nUWGnKLTnxqZ4XMeU1a8dna9fIN1WG3i+fBqsNHD8kRuKTS/0BibXAnnlK9FBak1onbg5fuqlSvsV41EscfEbP6g4IkXqVevTK8Ud5ihNSQ==";
       core.editor = "nvim";
       init.defaultBranch = "main";
       pull.rebase = true;
       push.autoSetupRemote = true;
       gpg.format = "ssh";
       "gpg \"ssh\"".program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
-      commit.gpgSign = false;
+      commit.gpgSign = true;
     };
   };
 
