5c9a6360af997a7d0b3ed429601e32197a5b91cd
Lucas Marcolongo <lucas_marco@live.com>
2025-08-22 15:55:19 -0700

    refactor(nvim-tree): standardize settings and action keys

--- modules/nixvim/plugins/nvim-tree.nix
+++ modules/nixvim/plugins/nvim-tree.nix
@@ -2,40 +2,42 @@
   plugins.nvim-tree = {
     enable = true;
     openOnSetup = true; # Don't auto-open, we'll use keybindings
-    disableNetrw = true;
-    hijackNetrw = true;
-    view = {
-      width = 30;
-      side = "left";
-    };
-    renderer = {
-      highlightGit = true;
-      icons = {
-        show = {
-          file = true;
-          folder = true;
-          folderArrow = true;
-          git = true;
+    settings = {
+      disable_netrw = true;
+      hijack_netrw = true;
+      view = {
+        width = 30;
+        side = "left";
+      };
+      renderer = {
+        highlight_git = true;
+        icons = {
+          show = {
+            file = true;
+            folder = true;
+            folder_arrow = true;
+            git = true;
+          };
         };
       };
-    };
-    actions = {
-      openFile = {
-        quitOnOpen = false;
-        resizeWindow = true;
+      actions = {
+        open_file = {
+          quit_on_open = false;
+          resize_window = true;
+        };
       };
-    };
-    git = {
-      enable = true;
-      ignore = false;
-    };
-    filters = {
-      dotfiles = false;
-      custom = [ ".git" "node_modules" ];
-    };
-    updateFocusedFile = {
-      enable = true;
-      updateRoot = false;
+      git = {
+        enable = true;
+        ignore = false;
+      };
+      filters = {
+        dotfiles = false;
+        custom = [ ".git" "node_modules" ];
+      };
+      update_focused_file = {
+        enable = true;
+        update_root = false;
+      };
     };
   };
   keymaps = [
